e354e8b4e4792ca5f91717f8727e626f9be60d24
Bobqian <bobqian1007@outlook.com>
2019-09-23 16:53:14 -0400

    lda working,not accurate

---
+++ project 1/mainTask.py
@@ -0,0 +1,57 @@
+import csv
+import numpy as np
+from model import LogisticRegression
+from model import LinearDiscriminantAnalysis
+
+
+
+def evaluate_acc(trainModel,validData):
+	testResults = []
+	for i in range(len(validData)):
+		testResults.append(trainModel.predict(validData[i,0:-1],validData[i,-1]))
+	n = 0
+	for i in testResults:
+		if i:
+			n = n + 1
+	print ('the accuracy is ',n/len(testResults) * 100,' %')
+	return n/len(testResults) * 100
+	
+def crossValidation(dataSet,trainModel):
+	numOfData = len(dataSet)
+	numOfDataPerSub = numOfData // 5 + 1
+	setNum = ((0,numOfDataPerSub - 1),(numOfDataPerSub, 2 * numOfDataPerSub - 1) ,(2 * numOfDataPerSub , 3 * numOfDataPerSub - 1),(3 * numOfDataPerSub, 4 * numOfDataPerSub - 1),(4 * numOfDataPerSub, len(dataSet) - 1))
+	sumAccuracy = 0
+	for i in setNum:
+		trainModel.fit(np.concatenate((dataSet[0:i[0],0:-1],dataSet[i[1]+1:len(dataSet),0:-1]),axis = 0),np.concatenate((dataSet[0:i[0],-1],dataSet[i[1]+1:len(dataSet),-1]),axis = 0))
+		sumAccuracy = sumAccuracy + evaluate_acc(trainModel,dataSet[i[0]:i[1] + 1,:])
+	sumAccuracy = sumAccuracy / 5
+	print('the total accuracy is ',sumAccuracy,' %')
+	return sumAccuracy
+	
+wines = []
+with open('winequality-red.csv','r') as f:
+	wines = list(csv.reader(f,delimiter=";"))
+wines = np.array(wines[1:],dtype=np.float)
+winesOnes = np.ones((len(wines),1))
+wines = np.concatenate((winesOnes,wines),axis = 1)
+for i in range(wines.shape[0]):
+	if wines[i,-1] > 5:
+		wines[i,-1] = 1
+	else:
+		wines[i,-1] = 0
+breastCancerList = []
+with open('breast-cancer-wisconsin.data','r') as f:
+	for line in f.readlines():
+		if not '?' in line:
+			l = list(map(int,line.strip().split(',')[1:]))
+			breastCancerList.append(l)
+breastCancers = np.array(breastCancerList,dtype=int)
+breastOnes = np.ones((len(breastCancers),1))
+breastCancers = np.concatenate((breastOnes,breastCancers),axis = 1)
+for i in range(len(breastCancers)):
+	breastCancers[i,-1] = (breastCancers[i,-1]-2)/2
+
+breastModel = LinearDiscriminantAnalysis()
+crossValidation(breastCancers,breastModel)
+winesModel = LogisticRegression(0.00009)
+crossValidation(wines,winesModel)
--- project 1/model.py
+++ project 1/model.py
@@ -2,35 +2,78 @@ import numpy as np
 import math as mt
 class LogisticRegression(object):
 	def __init__(self,updateRate):
-		self.weights = np.random.rand(1*12)*10
+		self.weights = np.random.rand(1,12)
 		self.updateRate = updateRate
 	def fit(self,inputData,resultData):
 		def featureSum(weights,inputData,resultData):
 			def alpha(result):
-				return 1/1+mt.exp(-result)    # 1 / ( 1+mt.exp(-result) ) ?
+				return 1/(1+mt.exp(-result))    # 1 / ( 1+mt.exp(-result) ) ?
 			zeros = np.zeros(inputData.shape[1])
 			for i in range(len(resultData)):
-				zeros = zeros + inputData[i]*(resultData - alpha(inputData[i].dot(weights.T)))
+				zeros = zeros + inputData[i]*(resultData[i] - alpha(inputData[i].dot(weights.T)))
 			return zeros
 		oldWeights =  self.weights
-		newWeights = oldWeights + updateRate * featureSum(oldWeights,inputData,resultData)
-		while not np.array_equals(oldWeights,newWeights):
+		newWeights = oldWeights + self.updateRate * featureSum(oldWeights,inputData,resultData)
+		while np.amax(np.abs(np.subtract(oldWeights,newWeights))) > 0.1 :
 			oldWeights = newWeights
-			newWeights = oldWeights + updateRate * featureSum(oldWeights,inputData,resultData)
+			newWeights = oldWeights + self.updateRate * featureSum(oldWeights,inputData,resultData)
 		self.weights = newWeights
 	def predict(self,inputData,trueLabel):		# split multi test sets into a few 1*m sets?
 		def alpha(result):
-			return 1/1+mt.exp(-result)
+			return 1/(1+mt.exp(-result))
 		output = 1 if (alpha(inputData.dot(self.weights.T)) > 0.5) else 0
 		return output == trueLabel
 
 class LinearDiscriminantAnalysis(object):
 	
-	def __init__(self)：
-		pass
+	def __init__(self):
+		self.mean0, self.mean1 = 0,0
+		self.n0, self.n1 = 0,0
+		self.P0,self.P1 = 0,0
+		self.cov_mat,self.inv_cov = 0,0
 
-	def fit(self,input,output):
+	def fit(self,inputData,resultData):		# this function takes complete data set. i.e. contain input & output in one set
+		input = np.concatenate((inputData,np.array([resultData]).T),axis = 1)
+		#print(input)
+		set0 = input[input[:,-1] == 0][:,:-1]
+		set1 = input[input[:,-1] == 1][:,:-1]
 
+		self.n0 = len(set0)
+		self.n1 = len(set1)
+
+		'''
+		if (n0+n1) != len(input):
+			#raise exception
+		'''
+
+		self.P0 = self.n0 / (self.n0 + self.n1)
+		self.P1 = self.n1 / (self.n0 + self.n1)
+
+	
+		self.mean0 = 1/self.n0 * np.sum(set0,axis=0)  #average all columns of set0 & set1
+		self.mean1 = 1/self.n1 * np.sum(set1,axis=0)  #average all columns of set0 & set1
+		
+		#print(self.mean0)
+		'''
+		cov_mat = np.cov((input[:,:-1]).T)
+		inv_cov = np.linalg.inv(cov_mat)
+		'''
+		def helper(input,mean):
+			sum = 0
+				#mean = comput_mean(input,output)
+				#n0,n1,total = comput_num(input,output)
+			total = self.n0 + self.n1
+			for i in range(len(input)):
+				x = input[i]			# x = the i-th row of input matrix
+				sum = sum + (x-mean)*((x-mean).T)/(total-2)
+			return sum
+			
+		self.cov_mat = self.cov_mat + helper(set0,self.mean0)
+		self.cov_mat = self.cov_mat + helper(set1,self.mean1)
+			
+		self.inv_cov = 1/self.cov_mat
+
+		'''
 		def compute_num(input,output):  # count the number of set(y=0),set(y=1) and total input sets
 			n0,n1 = 0,0
 			for i in range(len(output)):			# psuedo code
@@ -63,19 +106,32 @@ class LinearDiscriminantAnalysis(object):
 			sigma = sigma + helper(split(input,output,1))
 
 			return sigma
+			'''
 
 
 	def predict(self,input,output):
-
+		'''
 		n0,n1,total = fit.comput_num(input,output)
 		mean0 = fit.comput_mean( fit.split(input,output,0) )
 		mean1 = fit.comput_mean( fit.split(input,output,1) )
 		sigma = fit.comput_sigma(input,output)
 		x = input
+		'''
+		'''
+		bound = (np.log(self.P1/self.P0) 
+				-0.5 * np.dot(self.mean1, np.dot(inv_cov,mean1)) 
+				+ 0.5*np.dot(mean0,np.dot(inv_cov,mean0)) 
+				+ np.dot(input,np.dot(inv_cov,(mean0-mean1))) )
+		'''
+		bound = np.log(self.P1/self.P0) - 0.5 * np.dot((self.mean1).T, self.inv_cov * self.mean1) + 0.5*np.dot((self.mean0).T,self.inv_cov * self.mean0) + np.dot(input.T,self.inv_cov * (self.mean0-self.mean1)) 
+				
+		if bound > 0 :		# assume 2 represents y=0 branch. 4 represents y=1 branch
+			result = 1
+		else:
+			result = 0
+		
 
-		bound = mt.log((n1/n0),10) - 1/2*(mean1.T)*1/sigma*mean1 +1/2*(mean0.T)*1/sigma*mean0+(x.T)*1/sigma*(mean1-mean0)
-
-		return bound>0		# if bound>0 then y=1 else y=0
+		return result==output
 			
 
 
