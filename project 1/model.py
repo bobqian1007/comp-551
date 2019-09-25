ba814c1a445fcfe4467e7ae4be6b161306d8820e
Bobqian <bobqian1007@outlook.com>
2019-09-24 23:16:11 -0400

    yyc is niubi

--- project 1/model.py
+++ project 1/model.py
@@ -1,8 +1,8 @@
 import numpy as np
 import math as mt
 class LogisticRegression(object):
-	def __init__(self,updateRate):
-		self.weights = np.random.rand(1,12)
+	def __init__(self,updateRate,dataSet):
+		self.weights = np.ones(dataSet.shape[1]-1)
 		self.updateRate = updateRate
 	def fit(self,inputData,resultData):
 		def featureSum(weights,inputData,resultData):
@@ -13,10 +13,11 @@ class LogisticRegression(object):
 				zeros = zeros + inputData[i]*(resultData[i] - alpha(inputData[i].dot(weights.T)))
 			return zeros
 		oldWeights =  self.weights
-		newWeights = oldWeights + self.updateRate * featureSum(oldWeights,inputData,resultData)
-		while np.amax(np.abs(np.subtract(oldWeights,newWeights))) > 0.1 :
+		newWeights = oldWeights + self.updateRate * (featureSum(oldWeights,inputData,resultData)/len(inputData))
+		#while np.amax(np.abs(np.subtract(oldWeights,newWeights))) > 0.1 :
+		for i in range(5000):
 			oldWeights = newWeights
-			newWeights = oldWeights + self.updateRate * featureSum(oldWeights,inputData,resultData)
+			newWeights = oldWeights + self.updateRate * (featureSum(oldWeights,inputData,resultData)/len(inputData))
 		self.weights = newWeights
 	def predict(self,inputData,trueLabel):		# split multi test sets into a few 1*m sets?
 		def alpha(result):
@@ -26,17 +27,28 @@ class LogisticRegression(object):
 
 class LinearDiscriminantAnalysis(object):
 	
-	def __init__(self):
-		self.mean0, self.mean1 = 0,0
+	def __init__(self,input):
+		
+		m = input.shape[1]
+		#print("m:",m)
+
+		self.mean0, self.mean1 = np.zeros(m), np.zeros(m)
 		self.n0, self.n1 = 0,0
 		self.P0,self.P1 = 0,0
-		self.cov_mat,self.inv_cov = 0,0
+		self.cov_mat,self.inv_cov = np.zeros((m,m)), np.zeros((m,m))
+		#self.cov_mat,self.inv_cov = 0,0
 
 	def fit(self,inputData,resultData):		# this function takes complete data set. i.e. contain input & output in one set
-		input = np.concatenate((inputData,np.array([resultData]).T),axis = 1)
-		#print(input)
+		#print(resultData)
+		input = np.concatenate((inputData,np.transpose(np.array([resultData]))),axis = 1)
+
+		#print("input:\n",input)
+		
 		set0 = input[input[:,-1] == 0][:,:-1]
 		set1 = input[input[:,-1] == 1][:,:-1]
+
+		#print("set0:\n",set0)
+
 
 		self.n0 = len(set0)
 		self.n1 = len(set1)
@@ -53,11 +65,44 @@ class LinearDiscriminantAnalysis(object):
 		self.mean0 = 1/self.n0 * np.sum(set0,axis=0)  #average all columns of set0 & set1
 		self.mean1 = 1/self.n1 * np.sum(set1,axis=0)  #average all columns of set0 & set1
 		
-		#print(self.mean0)
+		
+		#cov_mat = np.cov((input[:,:-1]).T)
+		
+		def helper(input,mean):
+
+			m = input.shape[1]
+
+			sum = np.zeros((m,m))
+
+			total = self.n0 + self.n1
+
+			for i in range(len(input)):
+				#print(sum,"\n")
+				x = input[i]			# x = the i-th row of input matrix
+				#print((x-mean),"\n")
+				mat = x - mean
+				mat_t = mat.reshape(mat.shape[0],1)
+				#print(mat,"\n",mat_t)
+
+				sum = sum + mat_t*mat/(total-2)
+				#sum = sum + (np.transpose(x-mean))*(x-mean)/(total-2)
+			
+			return sum
+		
+		#print("set0,set1:",set0.shape[1], set1.shape[1])
+		#print("cov_mat:",self.cov_mat.shape[1])
+
+		self.cov_mat = self.cov_mat + helper(set0,self.mean0)
+		self.cov_mat = self.cov_mat + helper(set1,self.mean1)
+		
+		#print("cov_mat:\n",self.cov_mat)	
+		#print("det of cov_mat :",np.linalg.det(self.cov_mat))
+		self.inv_cov = np.linalg.inv(self.cov_mat)
+
 		'''
-		cov_mat = np.cov((input[:,:-1]).T)
-		inv_cov = np.linalg.inv(cov_mat)
-		'''
+		###############################################################
+		# initial method where cov_mat is scalar #
+
 		def helper(input,mean):
 			sum = 0
 				#mean = comput_mean(input,output)
@@ -72,59 +117,22 @@ class LinearDiscriminantAnalysis(object):
 		self.cov_mat = self.cov_mat + helper(set1,self.mean1)
 			
 		self.inv_cov = 1/self.cov_mat
-
+		###############################################################
 		'''
-		def compute_num(input,output):  # count the number of set(y=0),set(y=1) and total input sets
-			n0,n1 = 0,0
-			for i in range(len(output)):			# psuedo code
-				if output[i][0] == 0 :
-					n0 = n0 + 1
-				else
-					n1 = n1 + 1
-			total = no + n1
-			return n0,n1,total						
-
-		def split(input,output,y):		# this function is used to split input into two matrices according to y=0 or 1
-			pass						# return newInput,newOutput
-
-		def compute_mean(input,output):
-			return 1/(len(input)) * np.sum(input,axis=0)  #average all columns of input matrix
-
-		def compute_sigma(input,output)：
-
-			sigma = 0
-
-			def helper(input,output):
-				sum = 0
-				mean = comput_mean(input,output)
-				n0,n1,total = comput_num(input,output)
-				for i in range(len(input)):
-					x = input[i]			# x = the i-th row of input matrix
-					sum = sum + (x-mean)*((x-mean).T)/(total-2)
-
-			sigma = sigma + helper(split(input,output,0))
-			sigma = sigma + helper(split(input,output,1))
-
-			return sigma
-			'''
 
 
 	def predict(self,input,output):
-		'''
-		n0,n1,total = fit.comput_num(input,output)
-		mean0 = fit.comput_mean( fit.split(input,output,0) )
-		mean1 = fit.comput_mean( fit.split(input,output,1) )
-		sigma = fit.comput_sigma(input,output)
-		x = input
-		'''
-		'''
+		
+		
 		bound = (np.log(self.P1/self.P0) 
-				-0.5 * np.dot(self.mean1, np.dot(inv_cov,mean1)) 
-				+ 0.5*np.dot(mean0,np.dot(inv_cov,mean0)) 
-				+ np.dot(input,np.dot(inv_cov,(mean0-mean1))) )
+				-0.5 * np.dot(self.mean1, np.dot(self.inv_cov,np.transpose(self.mean1))) 
+				+ 0.5*np.dot(self.mean0,np.dot(self.inv_cov,np.transpose(self.mean0))) 
+				+ np.dot((self.mean1-self.mean0),np.dot(self.inv_cov,np.transpose(input))) )
+
 		'''
 		bound = np.log(self.P1/self.P0) - 0.5 * np.dot((self.mean1).T, self.inv_cov * self.mean1) + 0.5*np.dot((self.mean0).T,self.inv_cov * self.mean0) + np.dot(input.T,self.inv_cov * (self.mean0-self.mean1)) 
-				
+		'''
+
 		if bound > 0 :		# assume 2 represents y=0 branch. 4 represents y=1 branch
 			result = 1
 		else:
